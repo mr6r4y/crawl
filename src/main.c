e0094b5aeb5b7433249cd97cc7941bce21abb187
mr6r4y <mr6r4y@gmail.com>
2025-08-06 20:40:16 +0300

    Init

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#include "curl/curl.h"
+#include "curl/curlver.h"
+#include "curl/easy.h"
+#include "curl/header.h"
+#include "curl/mprintf.h"
+#include "curl/multi.h"
+#include "curl/options.h"
+#include "curl/stdcheaders.h"
+#include "curl/system.h"
+#include "curl/typecheck-gcc.h"
+
+int main(int argc, const char **argv)
+{
+	return 0;
+}
