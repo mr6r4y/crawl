7212d81dfcae1ae00b41c1750f0085e78201a483
mr6r4y <mr6r4y@gmail.com>
2025-08-17 15:50:25 +0300

    Add debug script

---
+++ .nvim-dap.lua
@@ -0,0 +1,32 @@
+local dap = require("dap")
+
+dap.adapters.gdb = function(cb, config)
+	cb({
+		id = "gdb",
+		type = "executable",
+		command = vim.fs.normalize("~/apps/binutils-gdb/bin/gdb"),
+		args = { "--quiet", "--interpreter=dap" },
+	})
+end
+
+dap.adapters.codelldb = function(cb, config)
+	cb({
+		id = "codelldb",
+		type = "executable",
+		command = "codelldb",
+	})
+end
+
+local prg = vim.fs.normalize("build/Debug/crawl")
+
+dap.configurations.c = {
+	{
+		-- The first three options are required by nvim-dap
+		type = "codelldb", -- the type here established the link to the adapter definition: `dap.adapters.python`
+		request = "launch",
+		name = "crawl-basic",
+
+		program = prg, -- This configuration will launch the current file if used
+		args = { "https://c-faq.com/decl/index.html" },
+	},
+}
